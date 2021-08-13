6cd0cbf2e9cdb6e94c9621db4d799adda2e51a68
ArantxaDR <arantxaderuiz@gmail.com>
2021-08-13 16:56:05 +0200

    feat: api call working

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,22 @@
 import React, { useEffect, useState }from 'react';
 // import getAllCountries from "../../services/CountriesData";
 // import { Route, Switch } from "react-router-dom";
+import {Country} from '../../common/interfaces/Country';
 import './App.scss';
 import Header from '../Header/Header';
 import CountriesList from '../Countries/list/CountriesList';
-// import { CountriesService } from '../../common/services/CountriesService';
+ import { countriesService } from '../../common/services/CountriesService';
 
 
 function App() {
 
-  // const [countries, setCountries] = useState([]);
+   const [countries, setCountries] = useState<Country[]>(); useState([]);
 
-  // useEffect(() => {
-  //   getAllCountries().then((data) => setCountries(data));
-  // }, []);
+  useEffect(() => {
+    countriesService.getAllCountries().then((data) => setCountries(data));
+  }, []);
+
+  console.log(countries);
 
   return (
     <main className="App">
--- src/components/Countries/card/CountriesCard.tsx
+++ src/components/Countries/card/CountriesCard.tsx
@@ -6,12 +6,7 @@ export default function CountriesCard() {
 	return (
 	<div className="card">
 		<img src="https://restcountries.eu/data/afg.svg" alt="Bandera del país" className="card__img" />
-		<ul className="card__content">
-			<li className="card__content-info">Nombre</li>
-			<li className="card__content-info">Capital</li>
-			<li className="card__content-info">Idioma</li>
-			<li className="card__content-info">Región</li>
-		</ul>
+		<p className="card__content">Nombre</p>
 		<div className="material-icons">star_outline</div>
 	</div>
 		
