d3ea5077cdb87635659b9658cba319d69227ec79
ArantxaDR <arantxaderuiz@gmail.com>
2021-08-16 12:15:41 +0200

    style: details page responsive css done

--- src/components/Countries/details/CountryDetail.tsx
+++ src/components/Countries/details/CountryDetail.tsx
@@ -4,36 +4,46 @@ import { Link } from "react-router-dom";
 import './_countryDetail.scss';
 
 export default function CountryDetail (props:any) {
+	if (props.country === undefined) {
+		return <h1>Ups! Wrong path</h1>;
+	}
 
 	const country =props.country;
 	
 	console.log(country);
 	return (
 		
-		<>
-        <Link to="/" className="home_link" title="Home l">
-          <button className="home">Home<i className="material-icons" >home</i></button>
+	<section className="details__wrapper">
+        <Link to="/" className="home_link" title="Home">
+          <button className="home">Home <i className="material-icons" >home</i></button>
         </Link>
-        <div className="card__detail">
-			<div className="card__detail-container">
-				<img 
-				className="card__detail-img" 
-				src={country.flag} 
-				alt={`${country.name} flag`} 
-				title={`${country.name} flag`}
-				/>
-			<h2 className="card_title">{country.name}</h2>
-            <ul className="card_detail__list">
-				<li>{`Capital: ${country.capital}`}</li>
-                <li>{`Region: ${country.region}`}</li>
-                <li>{`Subregion: ${country.subregion}`}</li>
-                <li>{`Population: ${country.population}`}</li>
-				<li>{`Area: ${country.area}`}</li>
-				<li>{`Currencies: ${country.currencies[0].name}`}</li>
-				<li>{`Borders: ${country.borders}`}</li>
-              </ul>
-            </div>
-        </div>
-      </>
+        <article className="detail__container">
+			<img
+			className="detail__img"
+			src={country.flag} 
+			alt={`${country.name} flag`} 
+			title={`${country.name} flag`}
+			/>
+			
+			<div className="country__details">
+				<div className="country__details-text">
+					<h2 className="country__details-text__title">{country.name}</h2>
+
+					<h5 className="country__details-text__subtitle">Capital: <span>{country.capital}</span></h5> 
+					<h5 className="country__details-text__subtitle">Region: <span>{country.region}</span></h5>
+					<h5 className="country__details-text__subtitle">Sub Region: <span>{country.subregion}</span></h5>
+					<h5 className="country__details-text__subtitle">Population: <span>{country.population}</span></h5>
+					<h5 className="country__details-text__subtitle">Area: <span>{country.area}</span></h5>
+				</div>
+				<div className="country__details-text">
+					<h5 className="country__details-text__subtitle">Currencies: <span>{country.currencies[0].name}</span></h5>
+					<h5 className="country__details-text__subtitle">Languages: <span>{country.languages[0].name}</span></h5>
+				
+				</div>
+				
+			</div>         
+                
+        </article>
+	</section>
 	)
 }
