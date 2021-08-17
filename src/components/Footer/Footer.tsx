a17184dd3b1424a11c29be91eea2974e85214813
ArantxaDR <arantxaderuiz@gmail.com>
2021-08-17 17:18:23 +0200

    style: made a sticky footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,8 +3,13 @@ import './_footer.scss'
 
 export default function Footer() {
 	return (
-		<small>
-			Made by ArantxaDR &copy; 2021
-		</small>
+	<div className="footer">
+		<p className="footer__text">
+		Made with 
+		<span className="material-icons footer_icon">
+		travel_explore</span>  
+		by ArantxaDR &copy; 2021
+		</p>
+	</div>
 	)
 }
