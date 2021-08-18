f16e18aa3fa6ee5b80b05e4b70d8cad1071b78d2
ArantxaDR <arantxaderuiz@gmail.com>
2021-08-18 20:55:58 +0200

    chore: change browserrouter for hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './components/App/App';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 
 ReactDOM.render(
