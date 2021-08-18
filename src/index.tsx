46cb917c9f3f7bb1ff1ea45c2986a9610781f24b
ArantxaDR <arantxaderuiz@gmail.com>
2021-08-18 21:27:46 +0200

    chore: add basename to router

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './components/App/App';
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 
 ReactDOM.render(
   <React.StrictMode>
-   <Router>
+   <Router basename="countryfinder">
       <App />
     </Router>
   </React.StrictMode>,
